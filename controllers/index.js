ace37b95f7acc836f4a7cd3b11fc3170e5d3282e
CoderGirl <angi.adema@gmail.com>
2023-09-11 12:26:43 -0600

    updated project-details.handlebars in order to include the homeRoutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', require('./homeRoutes.js'));
 
 module.exports = router;
