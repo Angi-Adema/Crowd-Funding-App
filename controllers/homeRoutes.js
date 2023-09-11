4a94dd8f28068ef6ae7364cc159186abad21a963
CoderGirl <angi.adema@gmail.com>
2023-09-10 19:48:09 -0600

    corrected loggedIn to logged_in on homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
         res.render('homepage', {
             projects,
-            loggedIn: req.session.logged_in
+            logged_in: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err);
