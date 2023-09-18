b16e8cd3379c63c9e85ab822c2a01a6e3712be44
CoderGirl <angi.adema@gmail.com>
2023-09-18 12:50:55 -0600

    updated userType in userRoutes.js to signify user vs admin

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -9,7 +9,7 @@ router.post('/', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.name = userData.name;
       req.session.logged_in = true;
-      req.session.userType = userData.userType
+      req.session.userType = userData.userType;
 
       res.status(200).json(userData);
     });
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.name = userData.name;
       req.session.logged_in = true;
-      req.session.userType = userData.userType
+      req.session.userType = userData.userType;
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
